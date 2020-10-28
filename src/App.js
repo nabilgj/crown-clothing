467513fd767de27d57a2d35b3083126434b71a63
nabilgj <nabilgj.bnm@gmail.com>
2020-10-28 13:30:03 -0400

    added react router dom and provided just base functionality in our menu item component where user now click hats page and hatspage component will get rendered

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import HomePage from "./pages/homepage/homepage";
 
+import { Switch, Route } from "react-router-dom";
+
+const HatsPage = () => (
+  <div>
+    <h1> HATS PAGE </h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage />
+      {/* <HomePage /> */}
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
