0dfe6393d1a85f3d7a7a98cc641f9956137732a6
nabilgj <nabilgj.bnm@gmail.com>
2020-10-30 16:51:46 -0400

    added redux persist library and now our state persist even on page refresh

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,21 @@
 import { combineReducers } from "redux";
 
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 
-// into store
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+
+// into store
+export default persistReducer(persistConfig, rootReducer);
